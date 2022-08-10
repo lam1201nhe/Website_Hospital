fc9107c2cd1cce91cada3681a7c0f70aba15770d
Tung Lam <mvvj2@MVVJ2s-MacBook-Pro.local>
2022-08-10 21:55:36 +0700

    Third commit

---
+++ JS/data.js
@@ -0,0 +1,38 @@
+const users = [];
+
+let name2 = document.getElementById("form3Example1cg");
+let submit = document.getElementById('submit-btn');
+submit.addEventListener('click', (event) => {
+    event.preventDefault();
+    let name = name2.value;
+    let phone = document.getElementById("form3Example2cg").value;
+    let email = document.getElementById("form3Example3cg").value;
+    let password = document.getElementById("form3Example4cg").value;
+    let user = {
+        "name": name,
+        "phone": phone,
+        "email": email,
+        "password": password,
+    }
+    users.push(user);
+    localStorage.setItem('users', JSON.stringify(users));
+    console.log(users[users.length - 1]);
+});
+
+//Check user empty
+function checkUserEmpty(name, phone, email, password) {
+    if (name == "" || phone == "" || email == "" || password == "") {
+        return false;
+    }
+    return true;
+}
+
+//Check local storage
+if(localStorage.getItem('users') === null){
+    localStorage.setItem('users', JSON.stringify(users));
+}
+
+
+
+
+
---
+++ JS/login.js
@@ -0,0 +1,33 @@
+let submitLogin = document.getElementById("submit-login");
+let alerts = document.getElementById("alert");
+let nameLogin = document.getElementById("form1Example13");
+let passLogin = document.getElementById("form1Example23");
+
+submitLogin.addEventListener("click", (event) => {
+  event.preventDefault();
+  userLogin();
+});
+let user = JSON.parse(localStorage.getItem("users"));
+
+function userLogin() {
+    let flag = false;
+  for (let e of user) {
+    if (nameLogin.value == e.phone || nameLogin.value == e.email) {
+      if (passLogin.value == e.password) {
+        flag=true;
+        break;
+      } 
+    }
+  }
+  if(flag){
+    alert("Login sucessfully!")
+    window.location.href="indexafterlog.html";
+  }else {
+    alerts.innerText = "Login failed";
+    alerts.style.color = "red";
+  }
+}
+
+
+
+
